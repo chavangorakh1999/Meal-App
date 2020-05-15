615ab805d4d5e05440f82a1d2484158a5a3abb54
chavangorakh1999 <chavangorakh19999@gmail.com>
2020-05-15 08:45:02 +0530

    starting App dev

--- mealApp/lib/main.dart
+++ mealApp/lib/main.dart
@@ -7,8 +7,27 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return MaterialApp(
+      title: 'DeliMeals',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('DesiKatta'),),
+      body: Center(child: Text('Starting Meal App'),),
     );
   }
 }
