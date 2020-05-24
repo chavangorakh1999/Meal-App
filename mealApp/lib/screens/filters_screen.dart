b9782a32ea27c05f95fbb76c4de803705bb36c18
chavangorakh1999 <chavangorakh19999@gmail.com>
2020-05-24 18:11:19 +0530

    using filter

--- mealApp/lib/screens/filters_screen.dart
+++ mealApp/lib/screens/filters_screen.dart
@@ -1,8 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:mealApp/widgets/main_drawer.dart';
 
-class FilterScreen extends StatelessWidget {
+class FilterScreen extends StatefulWidget {
   static const routeName = '/filter_screen';
+
+  @override
+  _FilterScreenState createState() => _FilterScreenState();
+}
+
+class _FilterScreenState extends State<FilterScreen> {
+  bool isGlutenFree = false;
+  bool isVegan = false;
+  bool isVegetarian = false;
+  bool isLactoseFree = false;
+
+  Widget _buildSwitchTile(
+    String title,
+    String description,
+    bool currentValue,
+    Function updateValue,
+  ) {
+    
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -10,6 +29,29 @@ class FilterScreen extends StatelessWidget {
           title: Text('Filters'),
         ),
         drawer: MainDrawer(),
-        body: Center(child: Text('Filter')));
+        body: Column(
+          children: <Widget>[
+            Container(
+              padding: EdgeInsets.all(20),
+              child: Text(
+                'Adjust your meal selection !',
+                style: Theme.of(context).textTheme.title,
+              ),
+            ),
+            Expanded(
+                child: ListView(
+              children: <Widget>[
+                SwitchListTile(
+                    title: Text('Gluten Free'),
+                    subtitle: Text('Include Gluten Free Meals'),
+                    onChanged: (newValue) {
+                      setState(() {
+                        isGlutenFree = newValue;
+                      });
+                    })
+              ],
+            ))
+          ],
+        ));
   }
 }
